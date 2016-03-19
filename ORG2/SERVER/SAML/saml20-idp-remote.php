f81183266020d9aa212fabe61a6ba5fa3a2dd35c
jlcs-es <joseluis.canovas@outlook.com>
2016-03-19 12:53:10 +0100

    SP org2 con IDP org1

---
+++ ORG2/SERVER/SAML/saml20-idp-remote.php
@@ -0,0 +1,16 @@
+<?php
+$metadata['http://192.168.31.100/simplesamlphp/saml2/idp/metadata.php'] = array (
+  'metadata-set' => 'saml20-idp-remote',
+  'entityid' => 'http://192.168.31.100/simplesamlphp/saml2/idp/metadata.php',
+  'SingleSignOnService' =>
+  array (
+    0 =>
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+      'Location' => 'http://192.168.31.100/simplesamlphp/saml2/idp/SSOService.php',
+    ),
+  ),
+  'SingleLogoutService' => 'http://192.168.31.100/simplesamlphp/saml2/idp/SingleLogoutService.php',
+  'certData' => 'MIIEHzCCAwegAwIBAgIJAMOSmytDWvyfMA0GCSqGSIb3DQEBCwUAMIGlMQswCQYDVQQGEwJFUzEPMA0GA1UECAwGTXVyY2lhMQ8wDQYDVQQHDAZNdXJjaWExHjAcBgNVBAoMFVVuaXZlcnNpZGFkIGRlIE11cmNpYTEgMB4GA1UECwwXQXNpZ25hdHVyYSBkZSBTZWd1cmlkYWQxFTATBgNVBAMMDHNlcnZlci5vcmczMTEbMBkGCSqGSIb3DQEJARYMemVraUB6ZXNuLmVzMB4XDTE2MDMwMzEyNTA0M1oXDTE3MDMwMzEyNTA0M1owgaUxCzAJBgNVBAYTAkVTMQ8wDQYDVQQIDAZNdXJjaWExDzANBgNVBAcMBk11cmNpYTEeMBwGA1UECgwVVW5pdmVyc2lkYWQgZGUgTXVyY2lhMSAwHgYDVQQLDBdBc2lnbmF0dXJhIGRlIFNlZ3VyaWRhZDEVMBMGA1UEAwwMc2VydmVyLm9yZzMxMRswGQYJKoZIhvcNAQkBFgx6ZWtpQHplc24uZXMwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDZ6re1M1Hf2utEdkn4JophsnPMzbZ59jtmBt24cz6NGIGQ1CJ+CBnBqQsIg0/fjthj2kKGe7M8JUJcMrYjQo9KG8qiYMlhMIYZKK/rbkWqxps7SZSGtm7RLsacU/oH7Yn2wbX90AUwklC2Dj4vGPGYlYPnx0v5zrhim8zf5F+NNwqwIpQsWNoHvG3eE+R3lcJQHpPGeC5hvN1EyIXZd7HZBusOW7d4r6ESfoZmKirzzy3OVkNguY+SyiO9BgGUyG0NB1Hd93PZe+BqDQS9+XsyHfxl9tpEQykgPUySwgTgBiJA/FX1q1krCjqfhYxQ5/6uo6o/ySiHZEYJTeQBVdg9AgMBAAGjUDBOMB0GA1UdDgQWBBTwlSw6jPImm1jBvORlDP+vMmkg2jAfBgNVHSMEGDAWgBTwlSw6jPImm1jBvORlDP+vMmkg2jAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQBsI21y+EzSBig1kHEETknN5Lven7VNesP0ecF27G6EuwvrDFK8bFDTajoH806Ay/GqPmOluCfvhj+tRrHoS+5q29pvKXfEDmpAI/CIzjbIHQKgFRegpSQkQEHn+x6KqoQdlEUUGwjuIV35h9rcYgkamfx0tU0CA+kcvymHxoDZkf8SMD/gxcWKuWxmEiApp4q2G8kihJW+68R9+m97B16Y9QBHysyEj05Q5wFG/FxPXjPRwwQOYgntQEcgvGzHw1txlQRlX21k3e/0Z7AUkDTqGflxX9rlD6Z4v+yCZ2da6Nw8sPSYT8AAIsX27NYOmA1rPUEKTKsJCYhd1GlTa8DQ',
+  'NameIDFormat' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
+);
---
+++ ORG2/SERVER/SAML/sp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp authsources.php /etc/simplesamlphp/authsources.php
+cp saml20-idp-remote.php /etc/simplesamlphp/metadata/saml20-idp-remote.php
+
+
